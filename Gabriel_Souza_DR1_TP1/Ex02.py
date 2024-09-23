9ae7b4611ed1b62c3dce760d88bfa929332e3c25
Gabriel Gomes <gabriel@mpsouza.com.br>
2024-09-23 01:02:28 -0300

    Criação pastas

--- Gabriel_Souza_DR1_TP1/Ex02.py
+++ Gabriel_Souza_DR1_TP1/Ex02.py
@@ -5,19 +5,18 @@ import random
 cartas_espadas = list(range(1, 14))
 random.shuffle(cartas_espadas)
 
-# Função para simular a ordenação manual das cartas utilizando Insertion Sort
 def insertion_sort(cartas):
-    # Simular a 2º mão pegando uma carta por vez e inserindo na posição correta na primeira mão
+    # Simular a 2º mão pegando uma carta por vez e inserindo na posição correta na 1º mão
     for i in range(1, len(cartas)):
-        carta_atual = cartas[i]  # A segunda mão pega a carta atual
+        carta_atual = cartas[i]  # A 2º mão pega a carta atual
         j = i - 1
         
-        # Mover as cartas da primeira mão para frente, se forem maiores que a carta atual
+        # Mover as cartas da 1º mão para frente, se forem maiores que a carta atual
         while j >= 0 and cartas[j] > carta_atual:
             cartas[j + 1] = cartas[j]  # Mover a carta para frente
             j -= 1
         
-        # Inserir a carta da segunda mão na posição correta
+        # Inserir a carta da 2º mão na posição correta
         cartas[j + 1] = carta_atual
 
 # Antes da ordenação
